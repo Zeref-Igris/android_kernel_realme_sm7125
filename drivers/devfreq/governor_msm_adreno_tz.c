3cd14c54976b57538c8890a0def8547637e16d0c
pri0818 <priyanshusinghal0818@gmail.com>
2022-03-01 08:17:35 +0000

    !fixup: Fix derps in 397e08d8c6d95fca8899b482d47dc10b51fc524a

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -64,12 +64,6 @@ static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
 
-static struct msm_adreno_extended_profile *partner_gpu_profile;
-static void do_partner_start_event(struct work_struct *work);
-static void do_partner_stop_event(struct work_struct *work);
-static void do_partner_suspend_event(struct work_struct *work);
-static void do_partner_resume_event(struct work_struct *work);
-
 static struct workqueue_struct *workqueue;
 
 static struct devfreq *tz_devfreq_g;
